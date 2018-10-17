7d9451bac1eba9ce72341933d416e58dc1d41670
annadowling <anna.dowling@errigal.com>
2018-10-17 22:45:06 +0100

    Added furtehr fix to white page error

--- src/main/java/com/wit/aws/root/RepositoryConfig.java
+++ src/main/java/com/wit/aws/root/RepositoryConfig.java
@@ -3,11 +3,12 @@ package com.wit.aws.root;
 import com.wit.aws.data.LocalJpaRepositoryConfig;
 import com.wit.aws.repositories.AWSRepository;
 import com.wit.aws.web.controllers.AWSController;
+import com.wit.aws.web.controllers.HomeController;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackageClasses = {AWSRepository.class, LocalJpaRepositoryConfig.class, AWSController.class})
+@ComponentScan(basePackageClasses = {AWSRepository.class, LocalJpaRepositoryConfig.class, AWSController.class, HomeController.class})
 public class RepositoryConfig {
 }
 
