91c867a60eb988037a1c9559569d878c3bb5c8fb
annadowling <anna.dowling@errigal.com>
2018-11-01 11:52:51 +0000

    Further request logging and id incrememting

--- src/main/java/com/wit/aws/web/controllers/AWSController.java
+++ src/main/java/com/wit/aws/web/controllers/AWSController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Optional;
 
@@ -15,6 +16,7 @@ import java.util.Optional;
 public class AWSController {
     private static final Logger logger = LoggerFactory.getLogger(AWSController.class);
     private AWSRepository repository;
+    private Integer requestToken = 0;
 
     @Autowired
     public AWSController(AWSRepository repository) {
@@ -22,31 +24,42 @@ public class AWSController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public Iterable<AWS> aws() {
+    public Iterable<AWS> aws(HttpServletRequest request) {
+        String ipAddress = IpHelper.getClientIp(request);
+        requestToken = new Integer(requestToken.intValue() + 1);
+        logger.info("REQUEST TOKEN ID IS: " + requestToken + " ,Request is:" + request.getProtocol() + " ,Spring-AWS App Received GET Request from Remote Ip Address: " + ipAddress + " , Getting All AWS Services");
         return repository.findAll();
     }
 
     @RequestMapping(method = RequestMethod.PUT)
-    public AWS add(@RequestBody @Valid AWS aws) {
-        logger.info("Adding AWS " + aws.getId());
+    public AWS add(@RequestBody @Valid AWS aws, HttpServletRequest request) {
+        String ipAddress = IpHelper.getClientIp(request);
+        requestToken = new Integer(requestToken.intValue() + 1);
+        logger.info("REQUEST TOKEN ID IS: " + requestToken + " ,Request is:" + request.getProtocol() + " ,Spring-AWS App Received POST Request from Remote Ip Address: " + ipAddress + " , Adding AWS Service: " + aws.getServiceName());
         return repository.save(aws);
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public AWS update(@RequestBody @Valid AWS aws) {
-        logger.info("Updating AWS Service " + aws.getId());
+    public AWS update(@RequestBody @Valid AWS aws, HttpServletRequest request) {
+        String ipAddress = IpHelper.getClientIp(request);
+        requestToken = new Integer(requestToken.intValue() + 1);
+        logger.info("REQUEST TOKEN ID IS: " + requestToken + " ,Request is:" + request.getProtocol() + " ,Spring-AWS App Received POST Request from Remote Ip Address: " + ipAddress + " , Adding AWS Service: " + aws.getServiceName());
         return repository.save(aws);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public Optional<AWS> getById(@PathVariable long id) {
-        logger.info("Getting AWS Service " + id);
+    public Optional<AWS> getById(@PathVariable long id, HttpServletRequest request) {
+        String ipAddress = IpHelper.getClientIp(request);
+        requestToken = new Integer(requestToken.intValue() + 1);
+        logger.info("REQUEST TOKEN ID IS: " + requestToken + " ,Request is:" + request.getProtocol() + " ,Spring-AWS App Received GET Request from Remote Ip Address: " + ipAddress + " , Getting AWS Service " + id);
         return repository.findById(id);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public void deleteById(@PathVariable long id) {
-        logger.info("Deleting AWS Service " + id);
+    public void deleteById(@PathVariable long id, HttpServletRequest request) {
+        String ipAddress = IpHelper.getClientIp(request);
+        requestToken = new Integer(requestToken.intValue() + 1);
+        logger.info("REQUEST TOKEN ID IS: " + requestToken + " ,Request is:" + request.getProtocol() + " ,Spring-AWS App Received DELETE Request from Remote Ip Address: " + ipAddress + " , Deleting AWS Service " + id);
         repository.deleteById(id);
     }
 }
--- src/main/java/com/wit/aws/web/controllers/IpHelper.java
+++ src/main/java/com/wit/aws/web/controllers/IpHelper.java
@@ -8,24 +8,25 @@ package com.wit.aws.web.controllers;/*******************************************
  *
  *************************************************************** */
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import javax.servlet.http.HttpServletRequest;
 
 /**
- * Created by annadowling on 17/10/2018.
+ * Created by annadowling on 01/11/2018.
  */
 
-@Controller
-public class HomeController {
-    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+public class IpHelper {
 
-    @RequestMapping("/spring-aws")
-    public String greeting(HttpServletRequest request) {
-        return "forward:/index.html";
+    public static String getClientIp(HttpServletRequest request) {
+
+        String remoteAddr = "";
+
+        if (request != null) {
+            remoteAddr = request.getHeader("X-FORWARDED-FOR");
+            if (remoteAddr == null || "".equals(remoteAddr)) {
+                remoteAddr = request.getRemoteAddr();
+            }
+        }
+
+        return remoteAddr;
     }
-
 }
